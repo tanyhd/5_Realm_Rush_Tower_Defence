482ed793f1ac23762c3f127b7409dcb69d16b3da
dtan_yh85 <darren243@hotmail.com>
2018-11-20 12:16:31 +0800

    Video 123 - [ExecuteInEditMode] Attribute

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour {
+    [Range(1f, 20f)][SerializeField] float gridSize = 10f;
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+	}
+}
