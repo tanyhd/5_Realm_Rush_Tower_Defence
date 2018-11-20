17592d473139ed063e4e99d5ec6a5f664622a28a
dtan_yh85 <darren243@hotmail.com>
2018-11-20 15:09:31 +0800

    Video 124 - Using Text Mesh For Labels

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour {
-    [Range(1f, 20f)][SerializeField] float gridSize = 10f;
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
 
-	// Update is called once per frame
-	void Update () {
+    [Range(1f, 20f)][SerializeField] float gridSize = 10f;
+    TextMesh textMesh;
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x/gridSize + "," + snapPos.z/gridSize;
 	}
 }
