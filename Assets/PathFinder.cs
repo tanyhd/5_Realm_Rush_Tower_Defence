b2c0e72d2a30e81f43afe5909155e66002ce8e09
dtan_yh85 <darren243@hotmail.com>
2018-11-20 22:25:55 +0800

    Video 130 - The Dictionary Data Structure

---
+++ Assets/PathFinder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour {
+
+    Dictionary<Vector2Int, WayPoint> grid = new Dictionary<Vector2Int, WayPoint>();
+
+	// Use this for initialization
+	void Start () {
+        LoadBlocks();
+		
+	}
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<WayPoint>();
+        foreach (WayPoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            if (grid.ContainsKey(gridPos))  // to check if the blocks are overlapping each other
+            {
+                Debug.LogWarning("Skipping overlapping block " + waypoint);
+            }
+            else
+            {
+                grid.Add(gridPos, waypoint);  // add to dictionary
+            }
+        }
+        print("Loaded " + grid.Count + " blocks");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Assets/WayPoint.cs
+++ Assets/WayPoint.cs
@@ -17,7 +17,7 @@ public class WayPoint : MonoBehaviour {
         return gridSize;
     }
 
-    public Vector2 GetGridPos()
+    public Vector2Int GetGridPos()
     {
         return new Vector2Int(
             Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
