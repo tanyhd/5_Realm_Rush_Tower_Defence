faf6185742e5539cb429251c37c6c3d2cf06ccdc
dtan_yh85 <darren243@hotmail.com>
2018-12-01 17:30:24 +0800

    Video 144 - Check For Distance

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +7,44 @@ public class Tower : MonoBehaviour {
 
     [SerializeField] List<Transform> objectToPan = new List<Transform>();
     [SerializeField] Transform targetEnemy;
+    [SerializeField] float attackRange = 10f;
+    [SerializeField] ParticleSystem projectileParticle;
 
-	// Update is called once per frame
-	void Update () {
-        foreach (Transform gameObjects in objectToPan)
+    // Update is called once per frame
+
+    void Update ()
+    {
+        if (targetEnemy)
         {
-            gameObjects.LookAt(targetEnemy);
+            foreach (Transform gameObjects in objectToPan)
+            {
+                gameObjects.LookAt(targetEnemy);
+            }
+            FireAtEnemy();
         }
-	}
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void FireAtEnemy()
+    {
+        float distanceToEnemy = Vector3.Distance(targetEnemy.transform.position, gameObject.transform.position);
+
+        if (distanceToEnemy <= attackRange)
+        {
+            Shoot(true);
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void Shoot(bool isActive)
+    {
+        var emissionModule = projectileParticle.emission;
+        emissionModule.enabled = isActive;
+    }
 }
