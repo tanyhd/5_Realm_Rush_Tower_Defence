feaaa9e7851feabf06e4fbb5785fca0dd0ec9e47
dtan_yh85 <darren243@hotmail.com>
2018-12-01 20:12:55 +0800

    Video 147 - Target Closest Enemy

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [Range(0.1f, 120f)]
+    [SerializeField] float secondsBetweenSpawns = 5f;
+    [SerializeField] GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(RepeatedlySpawnEnemies());
+    }
+
+    IEnumerator RepeatedlySpawnEnemies()
+    {
+        while (true)    // forever
+        {
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour {
 
+    // Parameters of each tower
     [SerializeField] List<Transform> objectToPan = new List<Transform>();
-    [SerializeField] Transform targetEnemy;
     [SerializeField] float attackRange = 10f;
     [SerializeField] ParticleSystem projectileParticle;
 
-    // Update is called once per frame
+    // State of each tower
+    Transform targetEnemy;
 
+    // Update is called once per frame
     void Update ()
     {
+        SetTargetEnemy();
+
         if (targetEnemy)
         {
             foreach (Transform gameObjects in objectToPan)
@@ -26,6 +30,33 @@ public class Tower : MonoBehaviour {
         {
             Shoot(false);
         }
+    }
+
+    private void SetTargetEnemy()
+    {
+        var sceneEnemies = FindObjectsOfType<EnemyDamage>();
+        if (sceneEnemies.Length == 0) { return; }
+
+        Transform closestEnemy = sceneEnemies[0].transform;
+
+        foreach (EnemyDamage testEnemy in sceneEnemies)
+        {
+            closestEnemy = GetClosest(closestEnemy, testEnemy.transform);
+        }
+
+        targetEnemy = closestEnemy;
+    }
+
+    private Transform GetClosest(Transform transformA, Transform transformB)
+    {
+        var disToA = Vector3.Distance(transform.position, transformA.position);
+        var disToB = Vector3.Distance(transform.position, transformB.position);
+
+        if (disToA > disToB)
+        {
+            return transformB;
+        }
+        return transformA;
     }
 
     private void FireAtEnemy()
